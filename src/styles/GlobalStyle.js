49db5d142a76e25af3295b78a728a7ca9e7c3a0f
mint <mintparc@icloud.com>
2021-11-22 19:23:38 +0900

    💄 Add a new font

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    @import url('https://fonts.googleapis.com/earlyaccess/notosanskr.css');
+    font-family: "Noto Sans KR", sans-serif !important;
+  }
+  
   body {
     color: ${({ theme }) => theme.fontColor};
     background: ${({ theme }) => theme.backgroundColor};
